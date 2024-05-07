8e96e75519ace92b5d1606d3e917098444c861cc
B0hdan2 <118682191+B0hdan2@users.noreply.github.com>
2024-05-07 04:36:50 +0300

    homework React-3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,10 +10,10 @@ function App() {
     { id: "id-3", name: "Eden Clements", number: "645-17-79" },
     { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
   ];
-  
+
   const [contacts, setContacts] = useState(() => {
     try {
-      const savedContacts = JSON.parse(window.localStorage.getItem('User'));
+      const savedContacts = JSON.parse(window.localStorage.getItem("User"));
       if (Array.isArray(savedContacts)) {
         return savedContacts;
       }
